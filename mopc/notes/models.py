ae910d8dce85f13df4c638b347a108af9e2d9c10
lidu <git@hebi.fr>
2021-06-20 14:59:08 +0300

    Day56

--- mopc/notes/models.py
+++ mopc/notes/models.py
@@ -2,20 +2,29 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from PIL import Image
 
-# defining a database table for posts
+
+class Category(models.Model):
+    label = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.label
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'pk': self.pk})
+
 class Note(models.Model):
-    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete = models.PROTECT, default='undefined')
+    title = models.CharField(max_length=255)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='',upload_to='images',blank=True,null=True)
-    audio = models.FileField(default='',upload_to='audio',blank=True,null=True)
+    image = models.ImageField(default='',upload_to='notes/images',blank=True,null=True)
+    doc = models.FileField(default='',upload_to='notes/docs',blank=True,null=True)
 
     def __str__(self):
         return self.title
 
-    # defining a URL for a new entry
     def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse('note_detail', kwargs={'pk': self.pk})
